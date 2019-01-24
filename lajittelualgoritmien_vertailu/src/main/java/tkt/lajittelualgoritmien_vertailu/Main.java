ed6256d7a97327477fa129b3d3ea1bf41920e023
Lauri Ranta <ranta.lauri@gmail.com>
2019-01-24 19:28:40 +0200

    Main toimii vihdoinkin

---
+++ lajittelualgoritmien_vertailu/src/main/java/tkt/lajittelualgoritmien_vertailu/Main.java
@@ -0,0 +1,16 @@
+package tkt.lajittelualgoritmien_vertailu;
+
+/**
+ *
+ * @author Lauri
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
